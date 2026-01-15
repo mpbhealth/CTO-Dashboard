e6600c2d14480b5e9b78d202dca9ebf6372cee95
Vinne R Tannous <system@mympb.com>
2026-01-15 16:40:03 -0500

    feat: Remove GalaxyDock and shell navigation components
    - Simplified AppShell to just pass through children
    - Removed GalaxyDock, CommandPalette, GalaxyMapModal from UI
    - Cleaned up dock-related CSS from index.css and globals.css
    - Removed pb-24 bottom padding since dock is gone
    - Kept shell component files for potential future use
    - Build verified successful with no errors

--- src/components/shell/index.ts
+++ src/components/shell/index.ts
@@ -1,13 +1,6 @@
 /**
- * Shell Components - CommandOS persistent UI layer
+ * Shell Components - Application wrapper
  */
 
 export { AppShell, useShell } from './AppShell';
-export { GalaxyDock } from './GalaxyDock';
-export { CommandPalette } from './CommandPalette';
-export { GalaxyMapModal } from './GalaxyMapModal';
-export { DockSearchBar } from './DockSearchBar';
-export { DockConfigModal } from './DockConfigModal';
-export { ExternalLinkItem, ExternalLinkGroup } from './ExternalLinkItem';
-export { QuickActionItem, QuickActionGroup } from './QuickActionItem';
 
